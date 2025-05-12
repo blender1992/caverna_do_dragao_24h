c4173855394089cada61bf0c383055f3dac0510d
blender1992 <movianserver2025@gmail.com>
2025-05-11 21:18:09 -0300

    Update m3u8.py

--- m3u8.py
+++ m3u8.py
@@ -7,11 +7,8 @@ PLAYLIST_FILE = "playlist.m3u8"
 @app.route('/channel')
 def channel():
     def generate():
-        while True:
-            with open(PLAYLIST_FILE, 'r') as f:
-                for line in f:
-                    yield line
-    return Response(generate(), mimetype='application/vnd.apple.mpegurl')
+        with open(PLAYLIST_FILE, 'r', encoding='utf-8') as f:
+            for line in f:
+                yield line
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    return Response(generate(), mimetype='application/vnd.apple.mpegurl')
